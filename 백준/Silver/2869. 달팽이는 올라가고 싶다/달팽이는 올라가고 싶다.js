f106a1f0bd6eebafcfaf6242bba628f7ae6c1c1c
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2022-09-26 19:13:41 +0900

    Update 달팽이는 올라가고 싶다.js
    기존 풀이방법 추가

--- 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -9,3 +9,25 @@ let [A,B,V] = fs
     
     const answer = Math.ceil((V - B) / (A - B))
     console.log(answer)
+
+/* 
+
+    기존 풀이방법
+    
+    let raisedRange = 0;
+    let dateToArrive = 0;
+    
+    if(A-B==1){
+        dateToArrive = V-B
+        console.log(dateToArrive)
+        } else {    
+            while(raisedRange<V){
+                raisedRange += A
+                dateToArrive++
+                if(raisedRange<V){
+                    raisedRange -= B
+                }
+            }
+            console.log(dateToArrive)
+            }
+*/
