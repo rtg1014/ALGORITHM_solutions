05047fa91a43d186fbfdb810bde67820e4290a95
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2024-04-06 00:07:18 +0900

    [level 1] Title: 둘만의 암호, Time: 3.52 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,28 @@
+function solution(s, skip, index) {
+    let answer = '';
+    const next_num = index 
+    
+    const skip_arr = skip.split('');
+    const target_arr = s.split('')
+    const alphbat_list = Array(26).fill().map((v, i) => String.fromCharCode(i + 97));
+    
+    let filter_alphbat_list = alphbat_list
+    
+    skip_arr.forEach((skip_word)=>{
+        const delete_word_index = filter_alphbat_list.indexOf(skip_word)
+        filter_alphbat_list.splice(delete_word_index, 1);
+    })
+    
+    const double_filter_alphbat_list = filter_alphbat_list.concat(filter_alphbat_list).concat(filter_alphbat_list)
+    for(let i=0; i<3; i++){
+        filter_alphbat_list.concat(filter_alphbat_list)
+    }    
+
+    console.log(double_filter_alphbat_list)
+    target_arr.forEach((word) =>{
+        const target_word_index = (alphbat_list.indexOf(word)) + next_num
+        answer += (double_filter_alphbat_list[target_word_index])
+    })
+    return answer;
+}
+
